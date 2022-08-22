0d94964d21b4d7351b0caefdff28c3b0c2eeb134
ruanvalente <ruan.valente@celk.net>
2022-08-22 17:16:46 -0300

    first commit

---
+++ pages/about/index.js
@@ -0,0 +1,7 @@
+export default function About() {
+  return (
+    <div>
+      <h2>Alura cases | FAQ</h2>
+    </div>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Alura Cases</h1>
+    </div>
+  );
+}
